c9199b58255bd68f09a882ceeed231840fa422ff
Johnbel <john@Johnbels-MacBook-Pro.local>
2021-12-15 20:24:31 -0500

    added dependecies for jwt

---
+++ Promise/src/main/java/com/promise/Promise/PromiseApplication.java
@@ -0,0 +1,13 @@
+package com.promise.Promise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromiseApplication.class, args);
+	}
+
+}
