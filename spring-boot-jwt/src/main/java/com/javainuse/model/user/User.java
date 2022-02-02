fdfdf215cc6d767db5fa750057a4c0a2e81f4d9e
Johnbel <john@johnbels-mbp.ssw.umb.edu>
2022-02-01 20:04:24 -0500

    Added User model

---
+++ spring-boot-jwt/src/main/java/com/javainuse/model/user/User.java
@@ -0,0 +1,43 @@
+package com.javainuse.model.user;
+
+public class User {
+
+	
+	private String Email;
+	private String Username;
+	private String Lastname;
+	private String Password;
+	private String Birthdate;
+	
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getLastname() {
+		return Lastname;
+	}
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getBirthdate() {
+		return Birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		Birthdate = birthdate;
+	}
+
+}
